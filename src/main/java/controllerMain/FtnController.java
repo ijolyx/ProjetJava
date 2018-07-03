86a74ba3ef8f4642315769f9c00aaf5f84f8d347
Alexy Joly <alexy.joly@epsi.fr>
2018-07-03 15:18:37 +0200

    ugoLeraleur

--- src/main/java/controllerMain/FtnController.java
+++ src/main/java/controllerMain/FtnController.java
@@ -2,32 +2,20 @@ package controllerMain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 
 
 import javax.servlet.http.HttpServlet;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.ProcessBuilder.Redirect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URI;
 import java.net.URL;
-import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.GET;
-import javax.ws.rs.core.UriBuilder;
-import model.Coord;
 import model.WeatherPojo;
 
 
